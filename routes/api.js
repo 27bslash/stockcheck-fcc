34f43d24c979a9cbfa5afb201b7a8d874031b8d1
Callum Kay <greenpear68@yahoo.co.ukcode - helpcode --helpcode --help>
2019-04-09 16:11:18 +0100

    Basic Api reqs done

--- routes/api.js
+++ routes/api.js
@@ -10,14 +10,26 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
-
+var request = require('request');
+// const place = 2019-09-05
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const url2 = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=YGIV1AV1PJXNF0J3';
+const url = 'https://api.iextrading.com/1.0//stock/AAPL/quote';
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(function (req, res) {
+      request(url, function (error, response, body) {
+
+        // var keysArray = Object.keys(body)
+        // for (var i = 0; i < keysArray.length; i++) {
+          // ['Meta Data']
+        // }
+        var obj = JSON.parse(body)
+        console.log(typeof(obj))
+        res.send(obj)
+      })
     });
-    
-};
+
+}
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -13,38 +13,46 @@ var server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-    
-    suite('GET /api/stock-prices => stockData object', function() {
-      
-      test('1 stock', function(done) {
-       chai.request(server)
+suite('Functional Tests', function () {
+
+  suite('GET /api/stock-prices => stockData object', function () {
+
+    test('request works', function (done) {
+      chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: 'goog'})
-        .end(function(err, res){
-          
+        .end(function (err, res) {
+          assert.notEqual(res.body, {})
+        })
+    });
+
+    test('1 stock', function (done) {
+      chai.request(server)
+        .get('/api/stock-prices')
+        .query({ stock: 'goog' })
+        .end(function (err, res) {
+
           //complete this one too
-          
+
           done();
         });
-      });
-      
-      test('1 stock with like', function(done) {
-        
-      });
-      
-      test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
-      });
-      
-      test('2 stocks', function(done) {
-        
-      });
-      
-      test('2 stocks with like', function(done) {
-        
-      });
-      
     });
+
+    test('1 stock with like', function (done) {
+
+    });
+
+    test('1 stock with like again (ensure likes arent double counted)', function (done) {
+
+    });
+
+    test('2 stocks', function (done) {
+
+    });
+
+    test('2 stocks with like', function (done) {
+
+    });
+
+  });
 
 });
